79e9faf5e52289dc159a56205a0c79a6e051d719
bougdar <bougdarahmed264@gmail.com>
2025-10-20 13:42:04 +0100

    fix updateAction

--- backend/controllers/auctionController.js
+++ backend/controllers/auctionController.js
@@ -35,22 +35,20 @@ export const getByIdAction = async (req, res) => {
 
 export const updateAction = async (req, res) => {
     try {
-        const { name, description, images, address, maxprice } = req.body;
-
-        if (!name || !description || !images || !address || !maxprice) {
-            return res.status(400).json({ message: "All fields are required." });
-        }
+        const { name, description, images, address, maxprice, auctionprice } = req.body;
 
         const existingAction = await Auction.findById(req.params.id);
         if (!existingAction) {
             return res.status(404).json({ message: "Action not found." });
         }
 
-        existingAction.name = name;
-        existingAction.description = description;
-        existingAction.images = images;
-        existingAction.address = address;
-        existingAction.maxprice = maxprice;
+        // Only update fields if they exist in the request
+        if (name) existingAction.name = name;
+        if (description) existingAction.description = description;
+        if (images) existingAction.images = images;
+        if (address) existingAction.address = address;
+        if (maxprice) existingAction.maxprice = maxprice;
+        if (auctionprice) existingAction.auctionprice = auctionprice;
 
         const updatedAction = await existingAction.save();
 
@@ -62,6 +60,7 @@ export const updateAction = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
 
 
 export const deletedAction = async (req, res) => {
